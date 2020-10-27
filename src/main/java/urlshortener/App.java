c1ab8ce3e5d4eb73bf8c0fc11a75222e266cfc9b
Sergio <alvarezpeirosergio@gmail.com>
2020-10-27 22:12:27 +0100

    Tratamiento de CSV  TODO: Hacer que devuelva archivos y no string

--- src/main/java/urlshortener/App.java
+++ src/main/java/urlshortener/App.java
@@ -16,15 +16,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
@@ -39,9 +39,7 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.ByteArrayOutputStream;
 
 
 @SpringBootApplication
@@ -144,6 +142,66 @@ public class App {
         }
     }
 
+    @PostMapping(value="/csv-file", produces="text/csv")
+    public ResponseEntity<String> shortener(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            // Parse file and get URL list
+            List<String> longURLs = getCSVUrls(file);
+            List<String> shorURLs = new ArrayList<>();
+            // Iterate the URL list
+            UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
+            for (String currentURL : longURLs) {
+                // Validate current URL
+                if (currentURL != null && urlValidator.isValid(currentURL) && this.urlExists(currentURL)) {
+                    // Generate the short URL
+                    String id = Hashing.murmur3_32().hashString(currentURL, StandardCharsets.UTF_8).toString();
+                    sharedData.opsForValue().set(id, currentURL);
+                    shorURLs.add(id);
+                }
+                else {
+                    shorURLs.add("invalidURL");
+                }
+            }
+            // Generate CSV file with the short URL list
+            try {
+                /*
+                TODO: Make this work
+                File f = new File("shortener.csv");
+                f.setReadable(true);
+                f.setWritable(true);
+                FileWriter csvWriter = new FileWriter(f);
+                for (int i = 0; i < shorURLs.size() - 1; ++i) {
+                    String newShortURL = shorURLs.get(i);
+                    csvWriter.append(newShortURL);
+                    csvWriter.append(",");
+                }
+                csvWriter.append(shorURLs.get(shorURLs.size()-1));
+                csvWriter.flush();
+                csvWriter.close();
+                 */
+                StringBuilder f = new StringBuilder();
+                for (int i = 0; i < shorURLs.size(); ++i) {
+                    String newShortURL = shorURLs.get(i);
+                    f.append(newShortURL);
+                    if (i != shorURLs.size() - 1) {
+                        f.append(",");
+                    }
+                }
+                // Generate ResponseEntity
+                HttpHeaders responseHeaders = new HttpHeaders();
+                responseHeaders.setContentType(MediaType.parseMediaType("text/csv"));
+                responseHeaders.setContentLength(f.length());
+                return new ResponseEntity<>(f.toString(),responseHeaders,HttpStatus.OK);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
     private String readFromRabbitMQ(String key) {
         try {
             String info = new String(this.channelMap.get(key).basicGet(key, false).getBody(), StandardCharsets.UTF_8);
@@ -193,5 +251,16 @@ public class App {
         }
     }
 
+    // Read the CSV and return a list of URLs.
+    private List<String> getCSVUrls (MultipartFile f) {
+        String fileContent = "";
+        try {
+            fileContent = new String(f.getBytes(), StandardCharsets.UTF_8);
+            fileContent = fileContent.replace("\n", "").replace("\r", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>(Arrays.asList(fileContent.split(",")));
+    }
 
 }
