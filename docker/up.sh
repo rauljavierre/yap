c7b94724861c9fe436e75d4bb6c3d027b9915628
rauljavierre <javierreraul@gmail.com>
2020-10-28 12:09:25 +0100

    bash up.sh se para si falla el build de gradle (fallan los tests)

--- docker/up.sh
+++ docker/up.sh
@@ -4,6 +4,11 @@ sudo bash down.sh
 
 cd ..
 gradle build # sudo snap install gradle --classic
+
+if [ $? -ne 0 ]; then
+  exit
+fi
+
 cd docker
 cp ../build/libs/urlshortener.jar spring-docker
 sudo docker-compose up --build
