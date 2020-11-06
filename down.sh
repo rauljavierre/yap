422c946433b081e5ec80b765949edede5dbbf7bc
rauljavierre <javierreraul@gmail.com>
2020-11-06 12:00:59 +0100

    hotfix down.sh

--- down.sh
+++ down.sh
@@ -7,5 +7,5 @@ sudo docker stop $(docker ps -a -q)
 sudo docker rm $(docker ps -a -q)
 sudo docker volume rm $(docker volume ls -q)
 
-sudo rm ./spring-docker/urlshortener.jar
+sudo rm ./spring-docker/YapShortener.jar
 
