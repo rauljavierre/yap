c54af581d6f713ceebce654d60d9c5b74cdcba73
rauljavierre <javierreraul@gmail.com>
2020-11-06 13:03:57 +0100

    60 seconds

--- up.sh
+++ up.sh
@@ -12,10 +12,11 @@ fi
 cp ./build/libs/YapShortener.jar spring-docker
 sudo docker-compose up --build --detach
 
+sleep 60
+
 pip3 install pika
 pip3 install psutil
 python3 src/main/python/get_host_information.py &
 
-sleep 60
 
 sudo gradle sonarqube
