b4d6a04c69d49df0ce2c5ce47f3ab5802fbfb8d0
rauljavierre <javierreraul@gmail.com>
2020-11-14 00:17:03 +0100

    RabbitMQ and Python removed from project

--- up.sh
+++ up.sh
@@ -10,21 +10,12 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-
 cp ./build/libs/YapShortener.jar spring-docker
 
 echo "[up.sh] Tearing up containers"
 sudo docker-compose up --build
 
 sleep 60
-
-echo "[up.sh] Tearing up Python script (get_host_information)"
-pip3 install pika
-pip3 install psutil
-pip3 install hurry.filesize
-python3 src/main/python/get_host_information.py &
-
-sleep 30
 
 echo "[up.sh] Building SonarQube"
 sudo gradle sonarqube
