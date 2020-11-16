90cddc6c5117a5284c61198a2692925c2d38f207
rauljavierre <javierreraul@gmail.com>
2020-11-16 13:23:53 +0100

    custom /metrics /info

--- src/main/java/urlshortener/controllers/InfoController.java
+++ src/main/java/urlshortener/controllers/InfoController.java
@@ -2,24 +2,32 @@ package urlshortener.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 @Component
-@Endpoint(id = "generated")  // /actuator/generated | /actuator/metrics
-public class GeneratedController {
+@EndpointWebExtension(endpoint = InfoEndpoint.class)
+public class InfoController {
 
     @Autowired
     private StringRedisTemplate constantsMap;
 
     @ReadOperation
-    public HashMap<String, String> getGenerated() {
+    public HashMap<String, String> getInfo() {
         HashMap<String, String> response = new HashMap<>();
         response.put("URLs", constantsMap.opsForValue().get("URLs"));
         response.put("QRs", constantsMap.opsForValue().get("QRs"));
         response.put("CSVs", constantsMap.opsForValue().get("CSVs"));
+
+        // https://www.dokry.com/9118
+        response.put("timestamp", ZonedDateTime.now( ZoneOffset.UTC ).format(DateTimeFormatter.ISO_INSTANT));
 
         return response;
     }
