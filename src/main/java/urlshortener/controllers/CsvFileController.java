c6a66e8633f2871bedae33a3f203246b594efb1d
Sergio <alvarezpeirosergio@gmail.com>
2020-11-18 12:22:16 +0100

    CSV file controller cumplimiento

--- src/main/java/urlshortener/controllers/CsvFileController.java
+++ src/main/java/urlshortener/controllers/CsvFileController.java
@@ -33,10 +33,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class CsvFileController {
 
     @Autowired
-    private StringRedisTemplate urlsMap;
-
-    @Autowired
-    private StringRedisTemplate constantsMap;
+    private StringRedisTemplate map;
 
     private final URLService urlService;
 
@@ -46,50 +43,48 @@ public class CsvFileController {
 
     @PostMapping(value="/csv-file", produces="text/csv")
     public ResponseEntity<String> shortener(@RequestParam("file") MultipartFile file, HttpServletRequest req) {
-        // TODO: test with future<String>...
-
         if(file.isEmpty()){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
+        boolean locationSet = false;
+        HttpHeaders responseHeaders = new HttpHeaders();
         List<String> longURLs = CsvUtils.getCSVUrls(file);
-        List<String> shortURLs = new ArrayList<>();
 
+        StringBuilder f = new StringBuilder();
         for (String currentURL : longURLs) {
+            f.append(currentURL);
+            f.append(",");
             Future<String> urlStatus = urlService.isValid(currentURL);
             try {
                 String urlStatusResult = urlStatus.get(1500, MILLISECONDS);
-                if(urlStatusResult.equals("URL is OK")) {
+                if (urlStatusResult.equals("URL is OK")) {
                     String id = Hashing.murmur3_32().hashString(currentURL, StandardCharsets.UTF_8).toString();
-                    shortURLs.add(id);
+                    urlService.insertURLIntoREDIS(id,currentURL,urlStatus);
+                    String newShortURL = req.getScheme() + "://" + req.getServerName() + "/" + id;
+                    f.append(newShortURL);
+                    f.append(",");
+                    if (!locationSet) {
+                        locationSet = true;
+                        responseHeaders.setLocation(URI.create(newShortURL));
+                    }
                 }
                 else {
-                    shortURLs.add(urlStatusResult);
+                    f.append(",");
+                    f.append(urlStatusResult);
                 }
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                shortURLs.add("URL not reachable");
+                f.append(",");
+                f.append("URL not reachable");
             }
+            f.append("\n");
         }
 
-        StringBuilder f = new StringBuilder();
-        for (int i = 0; i < shortURLs.size(); ++i) {
-            String newShortURL = req.getScheme() + "://" + req.getServerName() + "/" + shortURLs.get(i);
-            f.append(",");
-            f.append(longURLs.get(i));
-            f.append(newShortURL);
-            if (i != shortURLs.size() - 1) {
-                f.append("\n");
-            }
-        }
-
-        HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Content-Disposition", "attachment; filename=" + "shortener.csv");
         responseHeaders.setContentType(MediaType.parseMediaType("text/csv"));
         responseHeaders.setContentLength(f.length());
-        String urlLocation = req.getScheme() + "://" + req.getServerName() + "/" + shortURLs.get(0);
-        responseHeaders.setLocation(URI.create(urlLocation));
 
-        constantsMap.opsForValue().increment("CSVs");
+        map.opsForValue().increment("CSVs");
         return new ResponseEntity<>(f.toString(),responseHeaders,HttpStatus.CREATED);
     }
 }
