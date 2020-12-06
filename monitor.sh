6fe9dfb06cf7dd56b629c1a8c9f28ef27474d361
rauljavierre <javierreraul@gmail.com>
2020-12-06 17:33:05 +0100

    monitor.sh finished

--- monitor.sh
+++ monitor.sh
@@ -8,10 +8,11 @@ do
   echo "load: " $res
   echo "apps: "$number_of_apps
   if [ "$(echo " 5 < $res " | bc -l )" == 1 ] && [ "$number_of_apps" -eq "1" ]; then
-    echo "SCALE UP"
     number_of_apps=$((number_of_apps + 1))
+    sudo docker service scale yap_app=2
   elif [ "$(echo " 5 > $res " | bc -l )" == 1 ] && [ "$number_of_apps" -eq "2" ]; then
-    echo "SCALE DOWN"
     number_of_apps=$((number_of_apps - 1))
+    sudo docker service scale yap_app=1
   fi
+  sleep 1
 done
